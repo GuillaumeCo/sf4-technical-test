db09b53920f0f2d3c2e39c3d2cd782f4e7ee261b
GuillaumeCo <copinguillaume@hotmail.fr>
2018-01-29 03:15:42 +0100

    [+] TESTS : Start tests

--- tests/AppBundle/Controller/DefaultControllerTest.php
+++ tests/AppBundle/Controller/DefaultControllerTest.php
@@ -8,11 +8,27 @@ class DefaultControllerTest extends WebTestCase
 {
     public function testIndex()
     {
-        $client = static::createClient();
+        $client = static::createClient(array(), array(
+            'PHP_AUTH_USER' => 'username',
+            'PHP_AUTH_PW'   => 'pa$$word',
+        ));
 
         $crawler = $client->request('GET', '/');
 
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
-        $this->assertContains('Welcome to Symfony', $crawler->filter('#container h1')->text());
+        $this->assertContains('Rechercher un utilisateur GitHub', $crawler->filter('.panel-heading')->text());
+    }
+
+    public function testComment()
+    {
+        $client = static::createClient(array(), array(
+            'PHP_AUTH_USER' => 'username',
+            'PHP_AUTH_PW'   => 'pa$$word',
+        ));
+
+        $crawler = $client->request('GET', '/GuillaumeCo/comment');
+
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $this->assertContains('Commentaires pour les dépots de GuillaumeCo', $crawler->filter('h2')->text());
     }
 }
---
+++ tests/AppBundle/Service/GithubApiTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Tests\AppBundle\Service;
+
+use AppBundle\Service\GithubApi;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class GithubApiTest extends WebTestCase
+{
+    public function testSearchUser()
+    {
+        //$githubApiService = new GithubApi();
+    }
+}
